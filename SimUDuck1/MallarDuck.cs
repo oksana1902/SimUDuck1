fb7156ef0ef6c5444eb3e58d918fa9d3c66a28f9
Oksana1902 <os200215@gmail.com>
2022-01-20 22:27:30 +0300

    Добавьте файлы проекта.

---
+++ SimUDuck1/MallarDuck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck1
+{
+    public class MallarDuck : Duck
+    {
+        public override string display()
+        {
+            return "display" + this.GetType();
+        }
+    }
+}
